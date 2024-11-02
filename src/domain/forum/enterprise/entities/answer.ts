1b1426f16b69cfb29fec6065b8bd4f08c298d5f3
leonardo.reich <engsoft.leonardo@gmail.com>
2024-11-02 14:29:36 -0300

    event: response created

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,5 +1,6 @@
-import { Entity } from '@/core/entities/entity'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { AnswerCreatedEvent } from '@/core/events/answer-created-event'
 import { Optional } from '@/core/types/optional'
 import { AnswerAttachmentList } from '@/domain/forum/enterprise/entities/answer-attachment-list'
 
@@ -12,7 +13,7 @@ export interface AnswerProps {
    updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
    get authorId() {
       return this.props.authorId
    }
@@ -67,6 +68,12 @@ export class Answer extends Entity<AnswerProps> {
          },
          id,
       )
+
+      const isNewAnswer = !id
+
+      if (isNewAnswer) {
+         answer.addDomainEvent(new AnswerCreatedEvent(answer))
+      }
 
       return answer
    }
